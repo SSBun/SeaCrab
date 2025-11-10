0d5fc5d4385f2128ec7d10d4442ae2457ce8b1a3
CSL <caishilin@zhihu.com>
2025-11-10 11:48:02 +0800

    Initial Commit

---
+++ SeaCrab/SeaCrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCrabApp.swift
+//  SeaCrab
+//
+//  Created by caishilin on 2025/11/10.
+//
+
+import SwiftUI
+
+@main
+struct SeaCrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
